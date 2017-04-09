ea158014fe50ea61bbf4df979ae2bf28d8bc8a03
mjc-tiger <mj.cupial@gmail.com>
2017-04-09 18:56:05 +0200

    Add files via upload

---
+++ Portfolio/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#mephoto").addClass("animated fadeIn");
+  });
+
+$('.send').click(function(){
+  $('.form-item').addClass("animated hinge");
+});
+//when click send then hinge
